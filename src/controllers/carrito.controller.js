94cd0a44b888555f232545fd914dec3a733ed816
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2024-05-30 00:58:03 -0400

    Ruta para agregar pedidos y producto-pedido terminada

--- src/controllers/carrito.controller.js
+++ src/controllers/carrito.controller.js
@@ -5,35 +5,47 @@ const guardarPedidos = async (req, res) => {
   const pedido = req.body[0];
   const productoPedido = req.body[1];
   const formattedQueryPedidos = format(
-    `INSERT INTO %I (%I, %I) VALUES (${pedido.estadoPedido}, %L) RETURNING %I`,
+    `INSERT INTO %I (%I, %I, %I, %I) VALUES (%L, %L, %L, %L) RETURNING %I`,
     "pedidos",
+    "id usuarios",
+    "id direcciones",
     "estado pedido",
     "total",
+    pedido.idUsuario,
+    pedido.idDireccion,
+    pedido.estadoPedido,
     pedido.total,
     "id"
   );
 
-  const formattedQueries = await Promise.all(productoPedido.map(e => {
-    return format(
-      `INSERT INTO %I (%I, %I, %I, %I, %I) VALUES (%L, %L, %L, %L, %L)`,
-      "producto-pedidos",
-      "id_producto",
-      "id_pedidos",
-      "cantidad",
-      "precio_unitario",
-      "subTotal_producto",
-      e.id,
-      "10",
-      e.cantidad,
-      e.precioUnitario,
-      e.subTotal
-    );
-  }));
+  try {
+    const { rows } = await pool.query(formattedQueryPedidos);
+    const id = rows[0].id;
 
+    const formattedQueries = productoPedido.map(e => {
+      return format(
+        `INSERT INTO %I (%I, %I, %I, %I, %I) VALUES (%L, %L, %L, %L, %L)`,
+        "producto-pedidos",
+        "id producto",
+        "id pedidos",
+        "cantidad",
+        "precio unitario",
+        "subTotal producto",
+        e.id,
+        id,
+        e.cantidad,
+        e.precioUnitario,
+        e.subTotal
+      );
+    });
 
-
-  res.send(formattedQueries)
-}
+    const results = await Promise.all(formattedQueries.map(query => pool.query(query)));
+    res.json(results);
+  } catch (error) {
+    console.error("Error al guardar pedidos:", error);
+    res.status(500).json({ error: "Error al guardar pedidos" });
+  }
+};
 
 
 export const carritoController = {
