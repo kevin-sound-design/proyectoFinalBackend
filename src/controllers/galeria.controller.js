d93394dbf907c6fa035991674bb3a4757318b451
jcorp11 <joaco.011@gmail.com>
2024-05-31 23:22:04 -0400

    query de filtro

--- src/controllers/galeria.controller.js
+++ src/controllers/galeria.controller.js
@@ -1,20 +1,47 @@
-import pool from '../dataBase/conection.js';
-import format from 'pg-format';
+import pool from "../dataBase/conection.js";
+import format from "pg-format";
 
-const getProducts = async (req, res) =>{
+import { productModel } from "../models/product.model.js";
+
+const getProducts = async (req, res) => {
   const formattedQuery = format("SELECT * FROM %I", "producto");
-  const {rows} = await pool.query(formattedQuery);
+  const { rows } = await pool.query(formattedQuery);
   res.json(rows);
-}
+};
 
-const getProductById = async (req, res) =>{
+const getProductById = async (req, res) => {
   const productId = req.params.id;
-  const formattedQuery = format("SELECT * FROM %I WHERE id = %L", "producto", productId );
-  const {rows} = await pool.query(formattedQuery);
+  const formattedQuery = format(
+    "SELECT * FROM %I WHERE id = %L",
+    "producto",
+    productId
+  );
+  const { rows } = await pool.query(formattedQuery);
   res.json(rows);
-}
+};
+
+const getProductsByFilters = async (req, res) => {
+  const { minPrice, maxPrice, isNew, isUsed, categoria } = req.query;
+  try {
+    const products = await productModel.queryProductsByFilters({
+      minPrice,
+      maxPrice,
+      isNew,
+      isUsed,
+      categoria,
+    });
+
+    if (!products || products.length === 0)
+      return res.status(200).json({ message: "No products found" });
+    return res.status(200).json({ products });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const galeriaController = {
   getProducts,
-  getProductById
-}
+  getProductById,
+  getProductsByFilters,
+};
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,13 @@
 // models/product.model.js
 import { pool } from "../dataBase/conection.js";
 
-const createProduct = async ({ titulo, descripcion, precio, imagenUrl, stock }) => {
+const createProduct = async ({
+  titulo,
+  descripcion,
+  precio,
+  imagenUrl,
+  stock,
+}) => {
   const query = `
     INSERT INTO producto (titulo, descripcion, precio, "imagenUrl", stock, estado)
     VALUES ($1, $2, $3, $4, $5, true) RETURNING *;
@@ -11,7 +17,10 @@ const createProduct = async ({ titulo, descripcion, precio, imagenUrl, stock }) 
   return rows[0];
 };
 
-const updateProduct = async (id, { titulo, descripcion, precio, imagenUrl, stock, estado }) => {
+const updateProduct = async (
+  id,
+  { titulo, descripcion, precio, imagenUrl, stock, estado }
+) => {
   const query = `
     UPDATE producto
     SET titulo = $1, descripcion = $2, precio = $3, "imagenUrl" = $4, stock = $5, estado = $6
@@ -31,8 +40,47 @@ const deleteProduct = async (id) => {
   return rows[0];
 };
 
+const queryProductsByFilters = async ({
+  minPrice,
+  maxPrice,
+  isNew,
+  isUsed,
+  categoria,
+}) => {
+  let filtros = [];
+  const values = [];
+  const tableProducts = "producto";
+  const tableCategoriaProductos = '"categoria-producto"';
+  const tablaCategorias = "categorias";
+  const agregarFiltro = (campo, comparador, valor) => {
+    values.push(valor);
+    const { length } = filtros;
+    filtros.push(`${campo} ${comparador} $${length + 1}`);
+  };
+  if (maxPrice) agregarFiltro("a.precio", "<=", maxPrice);
+  if (minPrice) agregarFiltro("a.precio", ">=", minPrice);
+  if (categoria) agregarFiltro("c.categoria", "=", categoria);
+  if (isUsed && isNew) {
+    // agregarFiltro("a.estado", "in", "(true, false)");
+  } else if (isUsed) {
+    agregarFiltro("a.estado", "=", false);
+  } else if (isNew) {
+    agregarFiltro("a.estado", "=", true);
+  }
+  let consulta = `SELECT a.id, a.titulo, a.descripcion, a.precio, a."imagenUrl", a.stock, a.estado FROM ${tableProducts} a left join ${tableCategoriaProductos} b on a.id = b."id producto" left join ${tablaCategorias} c on b."id categoria" = c.id ${
+    filtros.length ? " WHERE " : ""
+  } ${filtros.join(" AND ")}`;
+
+  console.log(consulta);
+
+  const { rows } = await pool.query(consulta, values);
+  console.log(rows);
+  return rows;
+};
+
 export const productModel = {
   createProduct,
   updateProduct,
   deleteProduct,
+  queryProductsByFilters,
 };
