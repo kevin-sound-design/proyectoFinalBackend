e2ef8b605de6920008f2e4691983cf633fc3bb5f
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2024-06-05 20:40:11 -0400

    agregado connection string y ssl

--- src/dataBase/conection.js
+++ src/dataBase/conection.js
@@ -6,12 +6,14 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.PGUSER,
+  /* user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  allowExitOnIdle: true,
+  allowExitOnIdle: true, */
+  connectionString: "postgres://kevin:6NHiwgw11NgcvFbS5Z0YDFljPkb3pEMM@dpg-cpgf54cf7o1s738e32n0-a/proyecto_final_full_stack",
+  ssl:true
 });
 
 
