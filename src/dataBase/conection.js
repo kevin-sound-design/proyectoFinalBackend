fd8ae830478f22a61689a8b126a1ea90fd8729f6
DESKTOP-IS2G159\kevin <kevin.rodfa@gmail.com>
2024-06-07 00:54:35 -0400

    Arreglando coneccion de backend

--- src/dataBase/conection.js
+++ src/dataBase/conection.js
@@ -6,16 +6,16 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.PGUSER,
+  /* user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  allowExitOnIdle: true,
-  connectionString: process.env.EXTERNALURL,
+  allowExitOnIdle: true, */
+  connectionString: process.env.INTERNALURL,
   ssl: true
 });
 
-//pool.connect((err, client, release) => { if (err) { return console.error('Error adquiriendo cliente', err.stack); } console.log('Conectado a la base de datos'); release(); });
+pool.connect((err, client, release) => { if (err) { return console.error('Error adquiriendo cliente', err.stack); } console.log('Conectado a la base de datos'); release(); });
 
 export default pool;
